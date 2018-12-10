bfcf66ed5b4e5d9071d5c51681de73e74753dac0
Victor Yunusa <victoryunusa17@gmail.com>
2018-12-10 04:54:15 +0000

    Initial Commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+use Hash;
+use Session;
+use Validator;
+use Illuminate\Support\Facades\Input;
+use DB;
+use Auth;
+use Cloudder;
+
+class ProductsController extends Controller
+{
+    //
+
+
+}
---
+++ database/migrations/2018_12_10_035834_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('product_name');
+            $table->integer('category');
+            $table->string('price');
+            $table->text('description')->nullable();
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
